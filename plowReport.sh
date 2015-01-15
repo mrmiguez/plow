fa91789b734d3bb723c332658c71b883b69e67e1
mrmiguez <r.m.miguez@gmail.com>
2015-01-15 16:31:23 -0500

    function testing

--- plowReport.sh
+++ plowReport.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Define breaker function
+breaker () {
+	python ~/bin/dc_breaker/dc_breaker.py -e $1 $2
+}
+
 # Read date and start report
 iso=`date -I`
 touch fsudlReport$iso.csv
@@ -10,9 +16,10 @@ echo 'setSpec, # of IID' >>fsudlReport$iso.csv
 
 # Setting up the report loop
 for i in ${setList[@]}; do
-	echo $i "," `python ~/bin/dc_breaker/dc_breaker.py -e identifier ./harvest/$i* | wc -l` >>fsudlReport$iso.csv
+	echo "Analysing $i"
+	echo $i "," `breaker identifier ./harvest/$i* | wc -l` >>fsudlReport$iso.csv
 done
-printf "\n\nReport filed.\n"
+printf "\nReport filed.\n\n"
 	
 
 
