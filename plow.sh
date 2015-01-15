f12ab970968807b3e9362fb7decb0a71e55e3d2a
mrmiguez <r.m.miguez@gmail.com>
2015-01-15 11:51:07 -0500

    complete

--- plow.sh
+++ plow.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
+# Ready the output directory
+rm -rf ./harvest
+mkdir harvest
+
+
+# Reading set data into the array
 source setList.txt
+iso=`date -I`
+
+# Setting up the loop
 for i in ${setList[@]}; do
-	echo ${setList[i]}
+	# Set up the harvest
+	python ~/bin/pyoaiharvester/pyoaiharvest.py -l http://fsu.digital.flvc.org/oai2 -s $i -o harvest/$i$iso.xml
 done
+printf "\n\n#Harvest complete"
